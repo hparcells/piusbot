cd48de24d06edf9d09dd0cbddce0309b176ae031
Hunter Parcells <hparcells@icloud.com>
2019-08-21 14:39:15 -0400

    Cone Based Movement

--- main.js
+++ main.js
@@ -25,31 +25,29 @@ class PiusBot extends BaseAgent {
     this.renderer.beginRendering();
     
     // Right goal post.
-    this.renderer.drawLine3D(new Vector3(gameTickPacket.ball.physics.location.x, gameTickPacket.ball.physics.location.y, 17), new Vector3(-893 * teamMultiplier, 5120 * teamMultiplier, 17), new this.renderer.Color(0, 255, 0, 0));
-    const rightX = -893 * teamMultiplier - gameTickPacket.ball.physics.location.x;
-    const rightY = 5120 * teamMultiplier - gameTickPacket.ball.physics.location.y;
+    this.renderer.drawLine3D(new Vector3(gameTickPacket.ball.physics.location.x, gameTickPacket.ball.physics.location.y, 17), new Vector3(-893 * -teamMultiplier, 5120 * -teamMultiplier, 17), new this.renderer.Color(0, 255, 0, 0));
+    const rightX = (-893 * -teamMultiplier) - gameTickPacket.ball.physics.location.x;
+    const rightY = (5120 * -teamMultiplier) - gameTickPacket.ball.physics.location.y;
     const rightSlope = rightY / rightX;
-    const rightYIntercept = 5120 * teamMultiplier - rightSlope * (-893 * teamMultiplier);
+    const rightYIntercept = (5120 * -teamMultiplier) - rightSlope * (-893 * -teamMultiplier);
+
     this.renderer.drawLine3D(new Vector3(gameTickPacket.ball.physics.location.x, gameTickPacket.ball.physics.location.y, 17), new Vector3(
-      rightSlope > 0 ? -4096 * teamMultiplier : 4096 * teamMultiplier,
-      rightSlope * (rightSlope > 0 ? -4096 * teamMultiplier : 4096 * teamMultiplier) + rightYIntercept,
+      rightSlope > 0 ? -4096 * -teamMultiplier : 4096 * -teamMultiplier,
+      rightSlope * (rightSlope > 0 ? -4096 * -teamMultiplier : 4096 * -teamMultiplier) + rightYIntercept,
       17
     ), new this.renderer.Color(0, 0, 255, 0));
     
     // Left goal post.
-    this.renderer.drawLine3D(new Vector3(gameTickPacket.ball.physics.location.x, gameTickPacket.ball.physics.location.y, 17), new Vector3(893 * teamMultiplier, 5120 * teamMultiplier, 17), new this.renderer.Color(0, 255, 0, 0));
-    const leftX = 893 * teamMultiplier - gameTickPacket.ball.physics.location.x;
-    const leftY = 5120 * teamMultiplier - gameTickPacket.ball.physics.location.y;
+    this.renderer.drawLine3D(new Vector3(gameTickPacket.ball.physics.location.x, gameTickPacket.ball.physics.location.y, 17), new Vector3(893 * -teamMultiplier, 5120 * -teamMultiplier, 17), new this.renderer.Color(0, 255, 0, 0));
+    const leftX = (893 * -teamMultiplier) - gameTickPacket.ball.physics.location.x;
+    const leftY = (5120 * -teamMultiplier) - gameTickPacket.ball.physics.location.y;
     const leftSlope = leftY / leftX;
-    const leftYIntercept = 5120 * teamMultiplier - leftSlope * (893 * teamMultiplier);
+    const leftYIntercept = (5120 * -teamMultiplier) - leftSlope * (893 * -teamMultiplier);
     this.renderer.drawLine3D(new Vector3(gameTickPacket.ball.physics.location.x, gameTickPacket.ball.physics.location.y, 17), new Vector3(
-      leftSlope < 0 ? 4096 * teamMultiplier : -4096 * teamMultiplier,
-      leftSlope * (leftSlope < 0 ? 4096 * teamMultiplier : -4096 * teamMultiplier) + leftYIntercept,
+      leftSlope < 0 ? 4096 * -teamMultiplier : -4096 * -teamMultiplier,
+      leftSlope * (leftSlope < 0 ? 4096 * -teamMultiplier : -4096 * -teamMultiplier) + leftYIntercept,
       17
     ), new this.renderer.Color(0, 0, 255, 0));
-
-
-    this.renderer.drawString3D(new Vector3(carLocation.x, carLocation.y, carLocation.z + 100), 1, 1, `Position: ${(ballLocation.y - carLocation.y) * teamMultiplier > 0 ? 'Infront' : 'Behind'}`, new this.renderer.Color(255, 0, 255, 0));
 
     this.renderer.endRendering();
         
@@ -91,10 +89,22 @@ class PiusBot extends BaseAgent {
     
     // If we are behind the ball.
     if(teamMultiplier === -1 && ballLocation.y > carLocation.y || teamMultiplier === 1 && ballLocation.y < carLocation.y) {
-      if(inside([carLocation.x, carLocation.y], [
-        [893 * teamMultiplier, 5120 * teamMultiplier],
-        [-893 * teamMultiplier, 5120 * teamMultiplier],
-        [ballLocation.x, ballLocation.y]
+      if(ballLocation.x === 0 && ballLocation.y == 0) {
+        if(botFrontToTargetAngle > 0.1) {
+          controller.steer = 1;
+        }else if(botFrontToTargetAngle < -0.1) {
+          controller.steer = -1;
+        }else {
+          if(Math.max(ballLocation.y, carLocation.y) - Math.min(ballLocation.y, carLocation.y) < 350) {
+            forwardDodge();
+          }
+        }
+        controller.throttle = 1;
+        controller.boost = true;
+      }else if(inside([carLocation.x, carLocation.y], [
+        [ballLocation.x, ballLocation.y],
+        [rightSlope > 0 ? -4096 * -teamMultiplier : 4096 * -teamMultiplier, rightSlope * (rightSlope > 0 ? -4096 * -teamMultiplier : 4096 * -teamMultiplier) + rightYIntercept],
+        [leftSlope < 0 ? 4096 * -teamMultiplier : -4096 * -teamMultiplier, leftSlope * (leftSlope < 0 ? 4096 * -teamMultiplier : -4096 * -teamMultiplier) + leftYIntercept]
       ])) {
         if(botFrontToTargetAngle > 0.1) {
           controller.steer = 1;
@@ -106,50 +116,62 @@ class PiusBot extends BaseAgent {
           controller.boost = true;
           controller.throttle = 1;
 
-          if(ballLocation.y - carLocation.y < 350) {
+          if(Math.max(ballLocation.y, carLocation.y) - Math.min(ballLocation.y, carLocation.y) < 350) {
             forwardDodge();
           }
         }
       }else {
-        // -180
-        console.log(`${this.name}: ${Math.round(carRotation.yaw * (180 / Math.PI))}`);
-        if(carLocation.x * -teamMultiplier > ballLocation.x) {
-          if(Math.round(carRotation.yaw * (180 / Math.PI)) > -180) {
+        // TODO: Make this 10000000x better.
+        if((teamMultiplier === -1 && carLocation.x > ballLocation.x) || (teamMultiplier === 1 && carLocation.x < ballLocation.x)) {
+          if(botFrontToTargetAngle > -1.1) {
             controller.steer = 1;
-          }else {
-            controller.boost = true;
-          }
-          // 0
-        }else if(carLocation.x * -teamMultiplier < ballLocation.x) {
-          if(Math.round(carRotation.yaw * (180 / Math.PI)) > 0) {
+            controller.throttle = 0.5;
+          }else if(botFrontToTargetAngle < -0.9) {
             controller.steer = -1;
+            controller.throttle = 0.5;
+          }else {
+            controller.throttle = 1;
+          }
+        }else {
+          if(botFrontToTargetAngle > 1.1) {
+            controller.steer = 1;
+            controller.throttle = 0.5;
+          }else if(botFrontToTargetAngle < 0.9) {
+            controller.steer = -1;
+            controller.throttle = 0.5;
           }else {
             controller.boost = true;
+            controller.throttle = 1;
+  
+            if(Math.max(ballLocation.y, carLocation.y) - Math.min(ballLocation.y, carLocation.y) < 350) {
+              forwardDodge();
+            }
           }
         }
-        controller.throttle = 1;
       }
     }else {
       // If we infront of the ball.
-      this.renderer.drawString3D(new Vector3(carLocation.x - 200, carLocation.y, carLocation.z + 100), 1, 1, 'Rotating Back.', new this.renderer.Color(255, 0, 255, 0));
-
-      if(carLocation.x * -teamMultiplier > ballLocation.x) {
-        if(Math.round(carRotation.yaw * (180 / Math.PI)) > 80 * teamMultiplier || Math.round(carRotation.yaw * (180 / Math.PI)) < 100 * teamMultiplier) {
+      if((teamMultiplier === -1 && carLocation.x > ballLocation.x) || (teamMultiplier === 1 && carLocation.x < ballLocation.x)) {
+        if(Math.round(carRotation.yaw * (180 / Math.PI)) * teamMultiplier > 80 && Math.round(carRotation.yaw * (180 / Math.PI)) * teamMultiplier < 100) {
+          if(Math.round(carRotation.roll) === 0) {
+            forwardDodge();
+          }
+          if(!dodging && Math.round(carRotation.pitch) === 0) {
+            controller.boost = true;
+          }
+        }else {
           controller.steer = -1;
-        }else {
-          forwardDodge();
-          if(!dodging && Math.round(carRotation.pitch) === 0) {
-            controller.boost = true;
-          }
         }
-      }else if(carLocation.x * -teamMultiplier < ballLocation.x) {
-        if(Math.round(carRotation.yaw * (180 / Math.PI)) > 80 * teamMultiplier || Math.round(carRotation.yaw * (180 / Math.PI)) < 100 * teamMultiplier) {
-          controller.steer = 1;
-        }else {
-          forwardDodge();
+      }else {
+        if(Math.round(carRotation.yaw * (180 / Math.PI)) * teamMultiplier > 80 && Math.round(carRotation.yaw * (180 / Math.PI)) * teamMultiplier < 100) {
+          if(Math.round(carRotation.roll) === 0) {
+            forwardDodge();
+          }
           if(!dodging && Math.round(carRotation.pitch) === 0) {
             controller.boost = true;
           }
+        }else {
+          controller.steer = 1;
         }
       }
       controller.throttle = 1;
@@ -169,6 +191,11 @@ class PiusBot extends BaseAgent {
       if(carRotation.pitch < -0.5) {
         controller.pitch = 1;
       }
+    }
+
+    // If we are too high.
+    if(carLocation.z > 1000) {
+      controller.jump = true;
     }
 
     // If we score.
